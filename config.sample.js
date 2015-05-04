36d200f44fa510c96fcc6157f4418a73421abb43
Geoff Goodman <ggoodman@gmail.com>
2015-05-04 17:30:25 +0000

    Add sample config file

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+  auth: {
+    domain: 'auth0 domain',
+    clientID: 'auth0 client secret',
+    clientSecret: 'secret secret',
+  },
+  connection: {
+    host: "localhost",
+    port: 8080,
+  },
+  mongoUrl: "mongodb://localhost/webtasker",
+  webtaskApiUrl: "https://webtask.it.auth0.com/api",
+  webtaskContainer: "webtask container from webtask.io",
+  webtaskToken: "webtask token from webtask.io",
+  publicUrl: "http://locahost:8080",
+};
