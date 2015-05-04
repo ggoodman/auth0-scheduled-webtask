ed4358375729d463e5ff0399ccfdc7218d6b1e0c
Geoff Goodman <ggoodman@gmail.com>
2015-05-04 19:04:31 +0000

    Make timeout validation consistent

--- schema/task.js
+++ schema/task.js
@@ -6,5 +6,5 @@ module.exports = Joi.object().keys({
   secrets: Joi.object().pattern(/./, Joi.string()).optional().default({}),
   code: Joi.string().required(),
   schedule: Joi.string().regex(/^(((([\*]{1}){1})|((\*\/){0,1}(([0-9]{1}){1}|(([1-5]{1}){1}([0-9]{1}){1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([0-9]{1}){1}|(([1]{1}){1}([0-9]{1}){1}){1}|([2]{1}){1}([0-3]{1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([1-9]{1}){1}|(([1-2]{1}){1}([0-9]{1}){1}){1}|([3]{1}){1}([0-1]{1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([1-9]{1}){1}|(([1-2]{1}){1}([0-9]{1}){1}){1}|([3]{1}){1}([0-1]{1}){1}))|(jan|feb|mar|apr|may|jun|jul|aug|sep|okt|nov|dec)) ((([\*]{1}){1})|((\*\/){0,1}(([0-7]{1}){1}))|(sun|mon|tue|wed|thu|fri|sat)))$/).required(),
-  timeout: Joi.number().integer().min(1).max(60 * 5).optional().default(60 * 1), // Timeout in seconds
+  timeout: Joi.number().integer().min(10).max(60 * 60).optional().default(60 * 1), // Timeout in seconds
 });
